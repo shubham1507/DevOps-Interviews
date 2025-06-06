5d5271804f3b064f5cb7dc75fc19a1acc76d644b
shubham joshi <shubham.joshi1507@gmail.com>
2025-06-06 14:58:29 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,32 @@
+State Management & Collaboration:
+1. How do you manage Terraform state across environments? 
+2. How do you handle remote state locking in team setups? 
+3. What’s your strategy to recover from a corrupted state file? 
+4. How do you inspect/modify state without affecting infra?
+
+Modules & Reusability:
+5. How do you build reusable Terraform modules across teams? 
+6. How do you manage shared module versioning? 
+7. How do you deal with module dependencies and nesting?
+
+Advanced Logic & Operations:
+8. Difference between `count`, `for_each`, and `dynamic`. 
+9. When would you use `create_before_destroy` lifecycle rules? 
+10. Explain `terraform import` which you have used in any project . 
+11. Monorepo vs Polyrepo Terraform strategy – pros & cons.
+
+Security & Secrets Management:
+12. How do you secure secrets in Terraform without exposing them? 
+13. How do you integrate Azure Key Vault/Vault with Terraform? 
+14. How do you enforce RBAC for Terraform in CI/CD? 
+15. Tools you use to ensure Terraform security compliance?
+
+CI/CD & Automation: 
+16. How do you implement Terraform with GitOps? 
+17. How do you handle `terraform plan`/`apply` in pipelines? 
+18. What’s your approach to approval gates in prod applies?
+
+Debugging & Failures:
+19. How do you troubleshoot apply failures across modules? 
+20. How do you detect and resolve infra drift in Terraform? 
+21. What’s your rollback strategy if an apply breaks midway
