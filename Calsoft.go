10a841a0a9ed761e2148d951c9049dcf424a0481
shubham joshi <shubham.joshi1507@gmail.com>
2025-06-06 13:18:58 +0530

    Create Calsoft.go

---
+++ Calsoft.go
@@ -0,0 +1,25 @@
+#python code
+
+dict1={'name':'abc', 'email':'abc@xyz.com'}
+dict2=dict1
+del dict2['email']
+print dict1
+
+#python code
+class A:
+    def test(self):
+        print('A')
+
+class B:
+    def test(self):
+        print('B')
+
+class C(A, B):  # C inherits from A and B
+    pass
+
+obj = C()
+obj.test()
+
+# how to create APIS in python diff between patch and put ? can we implement patch using put and vice versa ? if we can than why ceated 2 methods
+# what is the use of Load Balancer
+# what are routines and channels in go
